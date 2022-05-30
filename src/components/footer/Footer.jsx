c7ab774abe7b2f30655c6e88403fb8b4cdaed180
GitBartbe <Bartosz.bednarski@wp.pl>
2022-05-30 02:28:16 +0200

    links added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,31 +1,29 @@
-import React from "react";
-import EmailForm from "../email-form/Email-form";
-import { BsGithub, BsLinkedin, BsFacebook } from "react-icons/bs";
-import "./footer.scss";
+import React from 'react';
+import EmailForm from '../email-form/Email-form';
+import { BsGithub, BsLinkedin, BsFacebook } from 'react-icons/bs';
+import './footer.scss';
 
 const Footer = () => {
   return (
-    <div className="footer" id="contact">
-      <div className="footer-content">
-        <div className="footer-left">
+    <div className='footer' id='contact'>
+      <div className='footer-content'>
+        <div className='footer-left'>
           <h2>You are welcome to meet me on:</h2>
-          <div className="logo-container">
-            <BsGithub className="footer-logo" size={40} />
-            <a href="./"> GitHub </a>
+
+          <div className='logo-container'>
+            <BsGithub className='footer-logo' size={40} />
+            <a href='https://github.com/GitBartbe'> GitHub </a>
           </div>
 
-          <div className="logo-container">
-            <BsLinkedin className="footer-logo" size={40} />
-            <a href="./"> LinkedIn </a>
-          </div>
-
-          <div className="logo-container">
-            <BsFacebook className="footer-logo" size={40} />
-            <a href="./"> Facebook </a>
+          <div className='logo-container'>
+            <BsLinkedin className='footer-logo' size={40} />
+            <a href='https://www.linkedin.com/in/bednarski-bartosz-front-end/'>
+              LinkedIn
+            </a>
           </div>
         </div>
 
-        <div className="footer-right">
+        <div className='footer-right'>
           <EmailForm />
         </div>
       </div>
